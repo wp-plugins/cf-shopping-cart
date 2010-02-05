154f20e51da0b6e71bcf6d67a940cb27d0268769
AI.Takeuchi <AI.Takeuchi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-05 04:35:14 +0000

    git-svn-id: https://plugins.svn.wordpress.org/cf-shopping-cart/trunk@202095 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- widget.php
+++ widget.php
@@ -4,7 +4,7 @@ Plugin Name: Cf Shopping Cart widget
 Plugin URI: http://takeai.silverpigeon.jp/
 Description: Placement simply shopping cart to content.
 Author: AI.Takeuchi
-Version: 0.1.2
+Version: 0.1.3
 Author URI: http://takeai.silverpigeon.jp/
 */
 
